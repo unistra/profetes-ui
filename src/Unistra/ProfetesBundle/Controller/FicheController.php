a527cd9496e34b53255ae658a3f6629d19159799
dbessey <dbessey@unistra.fr>
2013-07-25 16:17:04 +0200

    Affichage de la fiche diplôme avec le template

--- src/Unistra/ProfetesBundle/Controller/FicheController.php
+++ src/Unistra/ProfetesBundle/Controller/FicheController.php
@@ -10,8 +10,10 @@ class FicheController extends Controller
     public function indexAction($id)
     {
         $formation = $this->get('exist_db')->getResource($id);
+        $html = $formation->transform(__DIR__ . '/../Resources/xsl/fiche.xsl');
         return $this->render('UnistraProfetesBundle:Fiche:index.html.twig', array(
             'formation' => $formation,
+            'html'      => $html,
         ));
     }
 }
