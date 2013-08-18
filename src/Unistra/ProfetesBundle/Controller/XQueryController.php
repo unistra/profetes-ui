8e79c5948b9404428b869d36e40dfe0d01cf520c
dbessey <dbessey@unistra.fr>
2013-08-18 12:02:36 +0200

    Suppression de code inutile

--- src/Unistra/ProfetesBundle/Controller/XQueryController.php
+++ src/Unistra/ProfetesBundle/Controller/XQueryController.php
@@ -18,35 +18,11 @@ class XQueryController extends Controller
                 $this->container->getParameter('unistra_profetes.xquery.composante')),
             array('1' => $id));
         $xml = $exist_db->getXQuery($xquery);
-die(var_dump($xml));
+
         return $this->render('UnistraProfetesBundle:XQuery:composante.html.twig', array(
             'formations' => $xml,
             'composante' => $id,
         ));
     }
 
-
-    public function indexAction($id, $_format)
-    {
-        try {
-            $formation = $this->get('exist_db')->getResource($id);
-        } catch (\Exception $e) {
-            if (404 == $e->getCode()) {
-                throw $this->createNotFoundException();
-            } else {
-                throw new \Exception($e->getMessage());
-            }
-        }
-        $html = $formation->transform(
-            sprintf('%s/%s',
-                $this->container->getParameter('unistra_profetes.xsl.path'),
-                $this->container->getParameter('unistra_profetes.xsl.fiche')
-            )
-        );
-
-        return $this->render(sprintf('UnistraProfetesBundle:Fiche:index.%s.twig', $_format), array(
-            'formation' => $formation,
-            'html'      => $html,
-        ));
-    }
 }
