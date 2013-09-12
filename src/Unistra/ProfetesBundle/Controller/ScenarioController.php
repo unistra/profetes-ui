72c7bdaa40e18f6a57dba6836193249647e3b052
dbessey <dbessey@unistra.fr>
2013-09-12 16:00:47 +0200

    Implémentation des actions du premier scénario
     * Liste des types de diplômes
     * Liste des disciplines par type de diplôme
     * Formations par type de diplôme et discipline
    
    Pour la liste des formations, il reste à créer le XSL pour afficher la liste des
    formations. Ce sera la même pour les trois scenarios.

--- src/Unistra/ProfetesBundle/Controller/ScenarioController.php
+++ src/Unistra/ProfetesBundle/Controller/ScenarioController.php
@@ -3,6 +3,8 @@
 namespace Unistra\ProfetesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ScenarioController extends Controller
 {
@@ -10,6 +12,48 @@ class ScenarioController extends Controller
     {
         return $this->render('UnistraProfetesBundle:Scenario:index.html.twig');
     }
-}
 
+
+    public function listeTypesDiplomesAction()
+    {
+        $exist_db = $this->get('exist_db');
+        $xqueryPath = $this->container->getParameter('unistra_profetes.xquery.path');
+        $xquery = $exist_db->loadXQueryFromFile(
+            sprintf('%s/%s', $xqueryPath, 'liste-types-de-diplomes.xquery')
+        );
+        $exist_db->setCacheDir($xqueryPath . '/cache');
+        $xml = $exist_db->getXQuery($xquery, array('withXmlProlog' => false));
+
+        return new Response($xml);
+    }
+
+
+    public function disciplinesParTypeDeDiplomeAction($typeDeDiplome)
+    {
+        $exist_db = $this->get('exist_db');
+        $xqueryPath = $this->container->getParameter('unistra_profetes.xquery.path');
+        $xquery = $exist_db->loadXQueryFromFile(
+            sprintf('%s/%s', $xqueryPath, 'liste-disciplines-par-type-de-diplome.xquery'),
+            array('type-de-diplome' => $typeDeDiplome)
+        );
+        $exist_db->setCacheDir($xqueryPath . '/cache');
+        $xml = $exist_db->getXQuery($xquery, array('withXmlProlog' => false));
+
+        return new Response($xml);
+    }
+
+    public function parDisciplineEtTypeDeDiplomeAction($discipline, $typeDeDiplome)
+    {
+        $exist_db = $this->get('exist_db');
+        $xqueryPath = $this->container->getParameter('unistra_profetes.xquery.path');
+        $xquery = $exist_db->loadXQueryFromFile(
+            sprintf('%s/%s', $xqueryPath, 'par-type-de-diplome-et-discipline.xquery'),
+            array('type-de-diplome' => $typeDeDiplome, 'discipline' => $discipline));
+        $exist_db->setCacheDir($xqueryPath . '/cache');
+        $xml = $exist_db->getXQuery($xquery, array('withXmlProlog' => false));
+
+        return new Response($xml);
+    }
+
+}
 
