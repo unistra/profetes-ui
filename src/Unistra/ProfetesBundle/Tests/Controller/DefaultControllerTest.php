e33eaae9128948417d0c17e3bb2ad6f27a28c5e2
dbessey <dbessey@unistra.fr>
2014-04-14 09:03:12 +0200

    Test de la redirection pour /formations/
    Notamment après f6812912

--- src/Unistra/ProfetesBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Unistra/ProfetesBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,16 +4,23 @@ namespace Unistra\ProfetesBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @group   redirections
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/formations/');
-        $this->assertTrue($client->getResponse() instanceof RedirectResponse);
-        $this->assertRegExp('/www\.unistra\.fr/',
-            $client->getResponse()->headers->get('location'));
+        $response = $client->getResponse();
+        $this->assertTrue($response instanceof RedirectResponse);
+        $this->assertNotEquals(200, $response->getStatusCode());
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertRegExp('/www\.unistra\.fr\/index\.php\?id=etudes$/',
+            $response->headers->get('location'));
     }
 }
