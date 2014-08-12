d57f4cfe2d637c48289621002bfcc734e3525da2
dbessey <dbessey@unistra.fr>
2014-08-12 13:01:21 +0200

    Forme abrégée et correction, si possible de l'id

--- src/Unistra/UtilBundle/Command/CacheInfoCommand.php
+++ src/Unistra/UtilBundle/Command/CacheInfoCommand.php
@@ -22,13 +22,27 @@ class CacheInfoCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $tz = new \DateTimeZone('Europe/Paris');
-        $id = $input->getArgument('id');
+        $id = $this->normalizeId($input->getArgument('id'));
         $programId = new ProgramId($id);
         $cache = $this->getContainer()->get('unistra.profetes.cache.program_cache');
         $info = $cache->info($programId->getResourcePath());
 
-        $output->writeln('File : ' . $info['filename']);
-        $output->writeln('Date : ' . $info['updated']->setTimeZone($tz)->format('d/m/Y H:i:s P'));
-        $output->writeln('Size : ' . $info['size'] . ' o');
+        $output->writeln('<info>' . $id . '</info>');
+        $output->writeln('  File : ' . $info['filename']);
+        $output->writeln('  Date : ' . $info['updated']->setTimeZone($tz)->format('d/m/Y H:i:s P'));
+        $output->writeln('  Size : ' . $info['size'] . ' o');
+        $output->writeln('');
+    }
+
+    private function normalizeId($id)
+    {
+        $id = strtolower($id);
+        $id = str_replace(['_', '.', '/'], '-', $id);
+
+        if (preg_match("/^[a-z0-9]{3,}-\d+$/", $id)) {
+            $id = 'fr-rne-0673021v-pr-' . $id;
+        }
+
+        return $id;
     }
 }
