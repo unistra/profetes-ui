b986112f86f0da7ae2788b2d8a80ed63b2b0415c
dbessey <dbessey@unistra.fr>
2014-11-04 11:46:04 +0100

    Complété les tests de l'extension Twig XsltExtension

--- src/Unistra/ProfetesBundle/Tests/Twig/Extensions/XsltExtensionTest.php
+++ src/Unistra/ProfetesBundle/Tests/Twig/Extensions/XsltExtensionTest.php
@@ -6,17 +6,61 @@ use Unistra\ProfetesBundle\Twig\Extension\XsltExtension;
 
 class XsltExtensionTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testXsltTransform()
     {
-        $xslExt = new XsltExtension();
+        $xsltExtension = new XsltExtension();
 
-        $this->assertRegExp('/st/', 'test');
-        $this->assertRegExp('/<b>Test<\/b>/', $this->getXml());
-        $this->assertRegExp('/<p>Test<\/p>/', $xslExt->xsltTransform(
-            $this->getXml(),
-            $this->getXsl()
-        ));
+        $xml = $this->getXml();
+        $xsl = $this->getXsl();
+
+        $result = $xsltExtension->xsltTransform($xml, $xsl);
+
+        $this->assertXmlStringEqualsXmlString("<p>Test</p>", $result);
+    }
+
+    public function testXslTransformWithParameter()
+    {
+        $xsltExtension = new XsltExtension();
+
+        $xml = $this->getXml();
+        $xsl = $this->getXslWithParameter();
+
+        $result = $xsltExtension->xsltTransform($xml, $xsl, ['test-param' => 'Param value',]);
+
+        $this->assertXmlStringEqualsXmlString(
+            "<p>TestParam value</p>",
+            $result
+        );
+    }
+
+    public function testXslTransformWithDefaultParameter()
+    {
+        $xsltExtension = new XsltExtension();
+
+        $xml = $this->getXml();
+        $xsl = $this->getXslWithParameter();
+
+        $result = $xsltExtension->xsltTransform($xml, $xsl);
+
+        $this->assertXmlStringEqualsXmlString(
+            "<p>Testdefault-param-value</p>",
+            $result
+        );
+    }
+
+    public function testWithXslFile()
+    {
+        $xsltExtension = new XsltExtension();
+
+        $xml = $this->getXml();
+        $xsl = __DIR__.'/DataFixtures/template.xsl';
+
+        $result = $xsltExtension->xsltTransform($xml, $xsl);
+
+        $this->assertXmlStringEqualsXmlString(
+            '<p>Test</p>',
+            $result
+        );
     }
 
     protected function getXml()
@@ -39,4 +83,16 @@ class XsltExtensionTest extends \PHPUnit_Framework_TestCase
         return $xsl;
     }
 
+    protected function getXslWithParameter()
+    {
+        $xsl = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xsl .= '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">';
+        $xsl .= '<xsl:param name="test-param">default-param-value</xsl:param>';
+        $xsl .= '<xsl:template match="/">';
+        $xsl .= '<p><xsl:value-of select="/a/b" /><xsl:value-of select="$test-param"/></p>';
+        $xsl .= '</xsl:template>';
+        $xsl .= '</xsl:stylesheet>';
+
+        return $xsl;
+    }
 }
