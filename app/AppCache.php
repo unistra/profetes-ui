787b1412f6c268fb510018191f9b64740298f4ae
dbessey <dbessey@unistra.fr>
2014-11-05 09:25:08 +0100

    Mise en place du cache http

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,11 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+    protected function getOptions()
+    {
+        return [
+            'default_ttl' => 86400,
+            'allow_reload' => true,
+        ];
+    }
 }
